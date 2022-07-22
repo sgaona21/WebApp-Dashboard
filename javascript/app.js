5e925fcf5817590705f3860f97357fbdb224e330
sgaona21 <stevegaona21@gmail.com>
2022-07-21 17:10:47 -0700

    chart implementation

--- javascript/app.js
+++ javascript/app.js
@@ -8,3 +8,49 @@ closeButton.addEventListener('click', () => {
     alertBanner.style.display = "none";
 })
 
+
+
+// GRAPHS
+// LINE GRAPH
+const labels = [
+    '5:00am',
+    '9:00am',
+    '1:00pm',
+    '5:00pm',
+    '9:00pm',
+    '1:00am',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Hourly Traffic',
+      backgroundColor: 'rgb(39, 23, 177, 0.486)',
+      borderColor: 'rgb(39, 23, 177, 0.486)',
+      data: [445, 854, 4522, 12654, 9545, 1200, 665],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+
+// BAR GRAPH
+
+// PIE GRAPH
+
+
+
+// Traffic hourly daily weekly monhtly breakdowns
+const trafficSelector = document.getElementById('hourly-breakdown');
+trafficSelector.addEventListener('click', () => {
+    trafficSelector.style.backgroundColor = "lightgreen";
+    trafficSelector.style.borderRadius = "20px";
+})
--- test/toggle.js
+++ test/toggle.js
@@ -1,2 +1,31 @@
 console.log("linked")
 
+const labels = [
+    '5:00am',
+    '9:00am',
+    '1:00pm',
+    '5:00pm',
+    '9:00pm',
+    '1:00am',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Hourly Traffic',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [445, 854, 4522, 12654, 9545, 1200, 665],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
