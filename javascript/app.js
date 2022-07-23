3294f0b8281a8624189b731ef6f72912ec25ef5b
sgaona21 <stevegaona21@gmail.com>
2022-07-23 01:18:44 -0700

    js send fucntion

--- javascript/app.js
+++ javascript/app.js
@@ -163,3 +163,22 @@ monthlyBreakdown.addEventListener('click', () => {
     hourlyBreakdown.style.backgroundColor = "white";
     hourlyBreakdown.style.color = "black";
 })
+
+
+// Send Button
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+  // ensure user and message fields are filled out
+  if (user.value === "" && message.value === "") {
+  alert("Please fill out user and message fields before sending");
+  } else if (user.value === "" ) {
+  alert("Please fill out user field before sending");
+  } else if (message.value === "" ) {
+  alert("Please fill out message field before sending");
+  } else {
+  alert(`Message successfully sent to: ${user.value}`);
+  }
+  });
