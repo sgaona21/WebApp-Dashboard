ca86e7f8f4dc5dc6a12994e40c3fa6c4ec3db8f9
sgaona21 <stevegaona21@gmail.com>
2022-10-07 19:12:04 -0700

    notification disapears

--- javascript/app.js
+++ javascript/app.js
@@ -3,9 +3,11 @@ console.log("app.js linked");
 // ALERT BANNER 
 const closeButton = document.getElementById('x-button');
 const alertBanner = document.getElementById('alert')
+const greenNotification = document.getElementById("notification");
 
 closeButton.addEventListener('click', () => {
     alertBanner.style.display = "none";
+    greenNotification.style.display = "none";
 })
 
 
